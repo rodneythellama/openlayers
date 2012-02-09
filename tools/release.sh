c56788f9225a81e9f20c45ecfffdbb296137b3d6
Rodney Lorrimar <rodney@rodney.id.au>
2012-02-09 17:38:58 +0100

    build: update release.sh to build from a git checkout
    The script still supports svn, if a svn checkout is detected.
    
    This fixes #204.

--- tools/release.sh
+++ tools/release.sh
@@ -1,15 +1,50 @@
 #!/bin/sh
 
+if [ -z $1 ]; then
+    echo "Usage: $0 VERSION"
+    exit 1
+fi
+
 VERSION=$1
+TAG=release-$VERSION
+DIR=OpenLayers-$VERSION
+
+export_svn() {
+    svn export http://svn.openlayers.org/tags/openlayers/$TAG $DIR
+}
+
+export_git() {
+    if ! git describe $TAG > /dev/null 2>&1; then
+        echo "Tag $TAG not found. It needs to be an annotated tag."
+        exit 2
+    fi
+
+    # go to top level of work tree
+    cd `git rev-parse --show-cdup`
+
+    if [ -e tools/$DIR ]; then
+        echo "$DIR exists - please remove it"
+        exit 3
+    fi
+
+    mkdir tools/$DIR
+    git archive --format=tar $TAG | tar -xC tools/$DIR
+    cd tools
+}
 
 wget -c http://closure-compiler.googlecode.com/files/compiler-latest.zip
-unzip compiler-latest.zip 
+unzip -u compiler-latest.zip
+
+if svn info > /dev/null 2>&1; then
+    export_svn
+else
+    export_git
+fi
 
 wget -O release-${VERSION}.tar.gz https://github.com/openlayers/openlayers/tarball/release-${VERSION}
 tar xvzf release-${VERSION}.tar.gz
 mv openlayers-openlayers-* OpenLayers-${VERSION}
 cd OpenLayers-${VERSION}/build
-
 mv ../../compiler.jar ../tools/closure-compiler.jar
 ./build.py -c closure full
 ./build.py -c closure mobile OpenLayers.mobile.js
@@ -23,7 +58,7 @@ rm ../tools/closure-compiler.jar
 
 cd ..
 cd tools
-python exampleparser.py
+RELEASE_VER=$TAG python exampleparser.py
 cd ..
 for i in google ie6-style style; do
     csstidy theme/default/$i.css --template=highest theme/default/$i.tidy.css
